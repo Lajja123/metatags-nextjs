fee99208d137dd70ea4b3f71f8458de5d1bc6d9f
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2024-01-22 15:37:04 +0530

    update about us

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -23,3 +23,34 @@ function Aboutpage() {
 }
 
 export default Aboutpage;
+
+// Generate metadata for the page
+export const getMetadata = async function generateMetadata() {
+  // Extract params from props
+
+  return {
+    openGraph: {
+      title: "Next.js About us",
+      description: "About us The React Framework for the Web",
+      url: "https://metatags-nextjs.vercel.app/about",
+      siteName: "Next.js About us",
+      image:
+        "https://www.kasandbox.org/programming-images/avatars/old-spice-man.png",
+      images: [
+        {
+          url: "https://nextjs.org/og.png",
+          width: 800,
+          height: 600,
+        },
+        {
+          url: "https://nextjs.org/og-alt.png",
+          width: 1800,
+          height: 1600,
+          alt: "My custom alt",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+  };
+};
